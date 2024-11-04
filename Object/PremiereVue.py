1b010246face8c8170baccdaf55384cbc9544b6a
lololepro17 <123694004+lololepro17@users.noreply.github.com>
2024-11-04 11:26:32 +0100

    Update PremiereVue.py

--- Object/PremiereVue.py
+++ Object/PremiereVue.py
@@ -8,3 +8,10 @@ class Point:
         
     def distance(self,point):
         return sqrt((point.X - self.X) ** 2 + (point.Y - self.Y) ** 2)
+
+
+##Test##
+point1 = Point(3,4)
+point2 = Point(5,9)
+dist = point1.distance(point2)
+#print(dist)
