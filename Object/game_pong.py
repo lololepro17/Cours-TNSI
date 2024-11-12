abaacead6f9c16d4099893cfa2b20861dc702ca4
lololepro17 <123694004+lololepro17@users.noreply.github.com>
2024-11-12 11:46:18 +0100

    Update

--- Object/game_pong.py
+++ Object/game_pong.py
@@ -4,10 +4,11 @@ import math  # Pour utiliser cos et sin avec des angles
 # Dimensions de la fenêtre et du jeu
 LARGEUR_ECRAN = 160
 HAUTEUR_ECRAN = 120
-LARGEUR_RAQUETTE = 4
+LARGEUR_RAQUETTE = 1
 HAUTEUR_RAQUETTE = 20
 VITESSE_RAQUETTE = 3
-TROU_DU_BALLON = 1  # La marge pour marquer un point
+TROU_DU_BALLON = 10  # La marge entre la raquette et la balle
+SCORE_VICTOIRE = 5  # Score pour gagner le jeu
 
 # Classe pour le Joueur 1
 class Joueur1:
@@ -56,7 +57,7 @@ class Balle:
         self.y = HAUTEUR_ECRAN // 2
         self.vitesse = 2  # vitesse de déplacement de la balle
         self.angle = math.radians(45)  # angle initial en radians (par exemple, 45°)
-        self.taille = 4
+        self.taille = 2
 
     def deplacer(self):
         """Déplace la balle en fonction de l'angle et de la vitesse"""
@@ -70,13 +71,14 @@ class Balle:
 
     def afficher(self):
         """Affiche la balle à l'écran"""
-        pyxel.rect(self.x, self.y, self.taille, self.taille, 7)
+        pyxel.circ(self.x, self.y, self.taille, self.taille)
 
     def reset(self):
         """Réinitialise la balle au centre de l'écran et inverse sa direction"""
         self.x = LARGEUR_ECRAN // 2
         self.y = HAUTEUR_ECRAN // 2
-        self.angle = -self.angle  # Change de direction
+        self.angle = math.radians(45)  # Redémarre avec un angle défini
+        self.vitesse = 2  # Réinitialise la vitesse
 
 # Classe principale du Jeu Pong
 class Jeu:
@@ -103,13 +105,18 @@ class Jeu:
         self.balle.deplacer()
 
         # Collision avec les raquettes
+        # Pour le joueur 1
         if (self.balle.x <= self.joueur1.x + LARGEUR_RAQUETTE and
-            self.joueur1.y < self.balle.y < self.joueur1.y + HAUTEUR_RAQUETTE):
+            self.joueur1.y < self.balle.y + self.balle.taille < self.joueur1.y + HAUTEUR_RAQUETTE):
+            # Inversion horizontale et ajustement de l'angle en fonction de la position de la balle
             self.balle.angle = math.pi - self.balle.angle  # Inversion horizontale
 
-        if (self.balle.x >= self.joueur2.x - self.balle.taille and
-            self.joueur2.y < self.balle.y < self.joueur2.y + HAUTEUR_RAQUETTE):
+        # Pour le joueur 2
+        if (self.balle.x + self.balle.taille >= self.joueur2.x and
+            self.joueur2.y < self.balle.y + self.balle.taille < self.joueur2.y + HAUTEUR_RAQUETTE):
+            # Inversion horizontale et ajustement de l'angle en fonction de la position de la balle
             self.balle.angle = math.pi - self.balle.angle  # Inversion horizontale
+
 
         # Vérifie si un joueur a marqué un point
         if self.balle.x < TROU_DU_BALLON:
@@ -119,6 +126,10 @@ class Jeu:
         if self.balle.x > LARGEUR_ECRAN - TROU_DU_BALLON:
             self.joueur1.score += 1
             self.balle.reset()
+
+        # Fin du jeu si un joueur atteint le score de victoire
+        if self.joueur1.score >= SCORE_VICTOIRE or self.joueur2.score >= SCORE_VICTOIRE:
+            self.afficher_victoire()
 
     def draw(self):
         """Affiche les éléments du jeu pour chaque image"""
@@ -138,6 +149,24 @@ class Jeu:
         # Quitte le jeu quand on appuie sur Q
         if pyxel.btn(pyxel.KEY_Q):
             pyxel.quit()
+
+    def afficher_victoire(self):
+        """Affiche un message de victoire et attend que l'utilisateur appuie sur Q pour quitter"""
+        print('appel victoire')
+        pyxel.cls(0)
+        
+        # Affichage du message de victoire
+        if self.joueur1.score > self.joueur2.score:
+            pyxel.text(LARGEUR_ECRAN // 2 - 30, HAUTEUR_ECRAN // 2 - 10, "Joueur 1 gagne!", 8)
+        else:
+            pyxel.text(LARGEUR_ECRAN // 2 - 30, HAUTEUR_ECRAN // 2 - 10, "Joueur 2 gagne!", 8)
+        
+        pyxel.text(LARGEUR_ECRAN // 2 - 30, HAUTEUR_ECRAN // 2 + 10, "Appuyez sur Q pour quitter", 7)
+        
+        # Attente de l'appui sur la touche Q pour quitter
+        if pyxel.btn(pyxel.KEY_Q):
+            pyxel.quit()
+
 
 # Lancement du jeu
 Jeu()
